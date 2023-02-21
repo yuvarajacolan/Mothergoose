be14575de71d030239fa97193d058fcf7a73790e
CIPL0653 <>
2023-02-21 18:35:57 +0530

    Folder structure modification pt-2

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,15 +1,17 @@
 
 import { router } from 'next/router'
 import {useEffect} from 'react' 
+import React from 'react'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  useEffect(() => {
-      router.push("auth/new");
-  }, [])
+  // useEffect(() => {
+  //     router.push("auth/new");
+  // }, [])
   return (
     <>
+    <h1>HELL WORLD</h1>
     </>
   )
 }
